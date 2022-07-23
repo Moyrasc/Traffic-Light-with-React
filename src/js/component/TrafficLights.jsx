82a0fe48ac527eb3e12eafef95660044a1b47330
Moyra_sc <noemisolac@gmail.com>
2022-07-23 16:25:45 +0000

    proyecto finalizado incluido bonus

--- src/js/component/TrafficLights.jsx
+++ src/js/component/TrafficLights.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 //create your first component
 const TrafficLights = () => {
-  //Refactorizando código:
+  //Refactorizando código: como repetía demasiado código con estas variables de estado,decidi hacerlo de otra manera las líneas 50/51/52 pertenecen a este primer intento
   //   const [greenLight, setColorGreen] = useState("");
   //   const [redLight, setColorRed] = useState("");
   //   const [yellowLight, setColorYellow] = useState("");
@@ -12,34 +12,41 @@ const TrafficLights = () => {
   const [color, setColor] = useState("");
 
   //variable de estado para mostrar/ocultar el color morado
-  const [active, setActive] = useState(false)
+  const [active, setActive] = useState(false);
 
-   // función que guarde el elemento "clickado"
-   const elementClick = (e) => {
-    const clicked = e.target.id;
-    setColor(clicked);
-  };
- 
+  // función que guarde el elemento "clickado" 
+    const elementClick = (e) => {
+      const clicked = e.target.id;
+      setColor(clicked);
+    };
+
   //función para mostrar/activar la luz morada
-  const handleActive = () =>{
-	setActive(!active)
-  }
+  const handleActive = () => {
+    setActive(!active);
+  };
 
-	//función ciclo de luces
-  const automaticLights = () =>{
- const colorLight = ["red","yellow","green"]
-	setInterval()
-
-  }
+  //   función ciclo de luces..
+  const automaticLights = () => {
+    let colorIndex = 0;
+    const colorLight = ["red", "yellow", "green"];
+    setInterval(() => {
+      if (colorIndex <= colorLight.length) {
+        setColor(colorLight[colorIndex]);
+        colorIndex++;
+      } else {
+        return false;
+      }
+    }, 1000);
+  };
 
   return (
     <div className="wrapper">
-		<div className="m-auto bg-black" id="traffic-light"></div>
+      <div className="m-auto bg-black" id="traffic-light"></div>
       <div className="container">
         <div
           // creo operador ternario para activar las luces
           className={"red " + (color === "red" ? "lightRedOn" : "")}
-          id="red"
+		  id="red"
           onClick={elementClick}
           // setColorRed("glow");
           // setColorGreen("");
@@ -47,29 +54,34 @@ const TrafficLights = () => {
         ></div>
         <div
           className={"yellow " + (color === "yellow" ? "lightYellowOn" : "")}
-          id="yellow"
+		  id="yellow"
           onClick={elementClick}
         ></div>
         <div
           className={"green " + (color === "green" ? "lightGreenOn" : "")}
-          id="green"
+			id="green"
           onClick={elementClick}
         ></div>
-			{/* Creo renderizado condicional para que la luz morada solo se muestre al "clickar" el botón */}
-		{
-			active && <div
-			className={"purple " + (color === "purple" ? "lightPurpleOn" : "")}
+        {/* Creo renderizado condicional para que la luz morada solo se muestre al "clickar" el botón */}
+        {active && (
+          <div
+            className={"purple " + (color === "purple" ? "lightPurpleOn" : "")}
 			id="purple"
-			onClick={elementClick}
-		  ></div>
-		}	
-        
-   
+            onClick={elementClick}
+          ></div>
+        )}
       </div>
-	  <div className="d-grid gap-2 d-md-flex justify-content-md-center" id="buttons">
-          <button className="btn-lights" onClick={handleActive}>Show me</button>
-		  <button className="btn-cycle">Click Me!</button>
-        </div>
+      <div
+        className="d-grid gap-2 d-md-flex justify-content-md-center"
+        id="buttons"
+      >
+        <button className="btn-lights" onClick={handleActive}>
+          Show Me
+        </button>
+        <button className="btn-cycle" onClick={automaticLights}>
+          Active Lights
+        </button>
+      </div>
     </div>
   );
 };
@@ -77,4 +89,3 @@ const TrafficLights = () => {
 export default TrafficLights;
 
 
-// conseguir crear Create one button that when you click on it, it cycles the traffic light selected color between red, green and yellow.
