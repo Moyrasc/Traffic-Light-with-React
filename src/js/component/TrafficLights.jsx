6ce621a8cbb6c81faf969ad793028191ca0def2b
Moyra_sc <noemisolac@gmail.com>
2022-07-23 10:04:12 +0000

    comienzo  bonus luz morada

--- src/js/component/TrafficLights.jsx
+++ src/js/component/TrafficLights.jsx
@@ -3,68 +3,68 @@ import { useState } from "react";
 
 //create your first component
 const TrafficLights = () => {
-	//Refactorizando código
-	//   const [greenLight, setColorGreen] = useState("");
-	//   const [redLight, setColorRed] = useState("");
-	//   const [yellowLight, setColorYellow] = useState("");
+  //Refactorizando código:
+  //   const [greenLight, setColorGreen] = useState("");
+  //   const [redLight, setColorRed] = useState("");
+  //   const [yellowLight, setColorYellow] = useState("");
 
-	// creo variable de estado
-	const [color, setColor] = useState("");
+  // creo variable de estado para los colores del semaforo
+  const [color, setColor] = useState("");
+  
+  // creo variable de estado para mostrar/ocultar el color morado
+  const [active, setActive] = useState(false)
 
-	// creo una función que guarde el elemento "clickado"
-	const elementClick = (e) => {
-		const clicked = e.target.id
-		setColor(clicked);
-	}
+  //función para mostrar/activar la luz morada
+  const handleActive = () =>{
+	setActive(!active)
+  }
 
-	return (
-		<div className="wrapper">
-			<div className="container">
-				<div
-					// creo operador ternario para activar las luces
-					className={"red " + (color === "red" ? "lightRedOn" : "")}
-					id="red"
-					onClick={elementClick}
-					// setColorRed("glow");
-					// setColorGreen("");
-					// setColorYellow("");
+  // creo una función que guarde el elemento "clickado"
+  const elementClick = (e) => {
+    const clicked = e.target.id;
+    setColor(clicked);
+  };
 
-				></div>
-				<div
-					className={"yellow " + (color === "yellow" ? "lightYellowOn" : "")}
-					id="yellow"
-					onClick={elementClick}
-					// setColorYellow("glow");
-					// setColorRed("");
-					// setColorGreen("");
-
-				></div>
-				<div
-					className={"green " + (color === "green" ? "lightGreenOn" : "")}
-					id="green"
-					onClick={elementClick}
-					// setColorGreen("glow");
-					// setColorYellow("");
-					// setColorRed("");
-
-				></div>
-				<div className="buttons">
-					<button className="btn-hidden"></button>
-				</div>
-			
-				<div
-					className={"purple " + (color === "purple" ? "lightPurpleOn" : "")}
-					id="purple"
-					onClick={elementClick}
-
-				></div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="wrapper">
+      <div className="container-fluid">
+        <div
+          // creo operador ternario para activar las luces
+          className={"red " + (color === "red" ? "lightRedOn" : "")}
+          id="red"
+          onClick={elementClick}
+          // setColorRed("glow");
+          // setColorGreen("");
+          // setColorYellow("");
+        ></div>
+        <div
+          className={"yellow " + (color === "yellow" ? "lightYellowOn" : "")}
+          id="yellow"
+          onClick={elementClick}
+        ></div>
+        <div
+          className={"green " + (color === "green" ? "lightGreenOn" : "")}
+          id="green"
+          onClick={elementClick}
+        ></div>
+			{/* Creo renderizado condicional para que la luz morada solo se muestre al "clickar" el botón */}
+		{
+			active && <div
+			className={"purple " + (color === "purple" ? "lightPurpleOn" : "")}
+			id="purple"
+			onClick={elementClick}
+		  ></div>
+		}	
+        
+        <div className="buttons">
+          <button className="btn-hidden" onClick={handleActive}>Show me</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TrafficLights;
-
 
 // crear boton "activa luz morada" con su corresponddiente evento
 // conseguir crear Create one button that when you click on it, it cycles the traffic light selected color between red, green and yellow.
